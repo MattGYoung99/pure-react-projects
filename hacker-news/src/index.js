c24aa5b2f91c5f1cc137728f30476f5a8d233b8e
Matt Young <mattgyoung99@gmail.com>
2018-06-11 02:39:34 -0500

    added component functionality

--- hacker-news/src/index.js
+++ hacker-news/src/index.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import moment from 'moment';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 
 
-
+const expPost = [
+    {
+        id: 1,
+        points: 230,
+        comments: 4,
+        title: 'This is why i am not suing the great government of america',
+        username: 'iLoveToLearn',
+        source: 'linkhere.ok',
+        updated_at: "2016-07-11 21:24:00"
+    }
+]
 const HeadNavBar = () => {
     const headNavItems = ['news','threads','comments','show','ask','jobs','submit'];
     return (
@@ -24,18 +35,68 @@ const HeadNavBar = () => {
     )
 }
 
-const UserName = () => {
-
-}
-
-const Time = ({ time }) => {
-    const timeString = moment(time).fromNow();
+const UserName = ({username}) => {
     return (
-        <span className="time">
-            {timeString}
-        </span>
+        <span className="user">{username}</span>
     )
 }
- 
-ReactDOM.render(<HeadNavBar/>, document.getElementById('root'));
+
+
+const ArticleTitle = ({title}) => {
+    return (
+        <span className="title">{title}</span>
+
+    )
+}
+
+const ArticlePoints = ({points}) => {
+   return (
+       <span className="points">{points}</span>
+    )
+}
+
+const ArticleSource = ({source}) => {
+    return (
+        <span className="source">{source}</span>
+    )
+}
+const Article = ({props}) => {
+    return (
+        <div className="article">
+                <div className="postHead">
+                    <span className="listNumber">{props[0].id}</span>
+                    <ArticleTitle title={props[0].title}/>
+                    <ArticleSource source={props[0].source}/>
+                </div>
+                <div className="postNavBar">
+                    <ArticlePoints points={props[0].points}/>
+                    <p>by</p>
+                    <UserName username={props[0].username}/>
+                    <Time time={props[0].updated_at}/>
+                    <p className="flag"><a href="#">| Flag | Hide</a></p>
+
+                </div>
+        </div>
+    )
+}
+const Time = ({ time }) => {
+    const timeString = moment(time).fromNow();
+        return (
+            <span className="time">
+                {timeString}
+            </span>
+        )
+}
+
+class HackerNews extends React.Component {
+    render () {
+        return (
+            <div>
+                <HeadNavBar/>,
+                <Article props={expPost}/>
+            </div>
+        )
+    }
+}
+ReactDOM.render(<HackerNews/>, document.getElementById('root'));
 registerServiceWorker();
