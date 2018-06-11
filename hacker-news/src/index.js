c0b73083caef1e53e9737bfb8c992fc5d341f82e
Matt Young <mattgyoung99@gmail.com>
2018-06-11 04:50:07 -0500

    added more components and styling. polished functionality

--- hacker-news/src/index.js
+++ hacker-news/src/index.js
@@ -9,11 +9,146 @@ const expPost = [
     {
         id: 1,
         points: 230,
-        comments: 4,
+        comments: 19,
         title: 'This is why i am not suing the great government of america',
         username: 'iLoveToLearn',
-        source: 'linkhere.ok',
+        source: 'source.link',
+        updated_at: "2016-012-11 21:24:00"
+    },
+    {
+        id: 2,
+        points: 82,
+        comments: 4,
+        title: `Boy, 24 loses life earning on crypto`,
+        username: 'elijah',
+        source: 'source.link',
+        updated_at: "2017-03-30 21:24:00"
+    },
+    {
+        id: 3,
+        points: 20,
+        comments: 6,
+        title: 'Boy slipped on a slipping slide and hit a rock',
+        username: 'babyman',
+        source: 'source.link',
+        updated_at: "2017-07-11 21:24:00"
+    },
+    {
+        id: 4,
+        points: 11,
+        comments: 2,
+        title: 'What the hell is happening?',
+        username: 'dices',
+        source: 'source.link',
+        updated_at: "2018-02-11 21:24:00"
+    },
+    {
+        id: 5,
+        points: 593,
+        comments: 80,
+        title: 'Poverty line increased 22% in the past 5 years',
+        username: 'humaneBoy',
+        source: 'source.link',
+        updated_at: "2018-01-19 21:24:00"
+    },
+    {
+        id: 6,
+        points: 180,
+        comments: 11,
+        title: 'Man creates cure for cancer',
+        username: 'omar',
+        source: 'source.link',
+        updated_at: "2018-01-01 21:24:00"
+    },
+    {
+        id: 7,
+        points: 67,
+        comments: 0,
+        title: 'Man was found confused in meeting',
+        username: 'ItsTheCurve',
+        source: 'source.link',
         updated_at: "2016-07-11 21:24:00"
+    },
+    {
+        id: 8,
+        points: 812,
+        comments: 112,
+        title: 'List of my top played spotify songs',
+        username: 'ohWoah',
+        source: 'source.link',
+        updated_at: "2018-05-11 21:24:00"
+    },
+    {
+        id: 9,
+        points: 230,
+        comments: 19,
+        title: 'This is why i am not suing the great government of america',
+        username: 'iLoveToLearn',
+        source: 'source.link',
+        updated_at: "2016-012-11 21:24:00"
+    },
+    {
+        id: 10,
+        points: 82,
+        comments: 4,
+        title: `Boy, 24 loses life earning on crypto`,
+        username: 'elijah',
+        source: 'source.link',
+        updated_at: "2017-03-30 21:24:00"
+    },
+    {
+        id: 11,
+        points: 20,
+        comments: 6,
+        title: 'Boy slipped on a slipping slide and hit a rock',
+        username: 'babyman',
+        source: 'source.link',
+        updated_at: "2017-07-11 21:24:00"
+    },
+    {
+        id: 12,
+        points: 11,
+        comments: 2,
+        title: 'What the hell is happening?',
+        username: 'dices',
+        source: 'source.link',
+        updated_at: "2018-02-11 21:24:00"
+    },
+    {
+        id: 13,
+        points: 593,
+        comments: 80,
+        title: 'Poverty line increased 22% in the past 5 years',
+        username: 'humaneBoy',
+        source: 'source.link',
+        updated_at: "2018-01-19 21:24:00"
+    },
+    {
+        id: 14,
+        points: 180,
+        comments: 11,
+        title: 'Man creates cure for cancer',
+        username: 'omar',
+        source: 'source.link',
+        updated_at: "2018-01-01 21:24:00"
+    },
+    {
+        id: 15,
+        points: 67,
+        comments: 0,
+        title: 'Man was found confused in meeting',
+        username: 'ItsTheCurve',
+        source: 'source.link',
+        updated_at: "2016-07-11 21:24:00"
+    },
+    {
+        id: 16,
+        points: 812,
+        comments: 112,
+        title: 'List of my top played spotify songs',
+        username: 'ohWoah',
+        source: 'source.link',
+        updated_at: "2018-05-11 21:24:00"
     }
 ]
 const HeadNavBar = () => {
@@ -34,52 +169,67 @@ const HeadNavBar = () => {
         </div>
     )
 }
-
 const UserName = ({username}) => {
     return (
-        <span className="user">{username}</span>
+        <span className="user">{`${username} `}</span>
     )
 }
-
-
 const ArticleTitle = ({title}) => {
     return (
         <span className="title">{title}</span>
 
     )
 }
-
 const ArticlePoints = ({points}) => {
    return (
-       <span className="points">{points}</span>
+       <span className="points">{`${points} points by `}</span>
     )
 }
-
 const ArticleSource = ({source}) => {
     return (
-        <span className="source">{source}</span>
+        <span className="source"><a href="#">{`(${source})`}</a></span>
     )
 }
-const Article = ({props}) => {
+const Comments = ({comments}) => {
     return (
-        <div className="article">
-                <div className="postHead">
-                    <span className="listNumber">{props[0].id}</span>
-                    <ArticleTitle title={props[0].title}/>
-                    <ArticleSource source={props[0].source}/>
-                </div>
-                <div className="postNavBar">
-                    <ArticlePoints points={props[0].points}/>
-                    <p>by</p>
-                    <UserName username={props[0].username}/>
-                    <Time time={props[0].updated_at}/>
-                    <p className="flag"><a href="#">| Flag | Hide</a></p>
-
-                </div>
+        <span className="comments">{`| ${comments} comments`}</span>
+    )
+}
+const ArrowsContainer = () => {
+    return (
+        <div className="arrowsContainer">
+            <div className="arrows">
+                <span className="upvote">&#9652;</span>
+                <span className="downvote">&#9652;</span>
+            </div>
         </div>
     )
 }
-const Time = ({ time }) => {
+const Article = ({posts}) => {
+    return (
+        posts.map(function(item, i) {
+            return (
+                <div className="article">
+                        <div className="postHead">
+                            <span className="listNumber">{`${posts[i].id}.`}</span>
+                            <ArrowsContainer/>
+                            <ArticleTitle title={posts[i].title}/>
+                            <ArticleSource source={posts[i].source}/>
+                        </div>
+                        <div className="postNavBar">
+                            <ArticlePoints points={posts[i].points}/>
+                            <UserName username={posts[i].username}/>
+                            <Time time={posts[i].updated_at}/>
+                            <span className="flag"> | <a href="#">Flag</a> | <a href="#">Hide</a></span>
+                            <Comments comments={posts[i].comments}/>
+                            <span className="save"> | <a href="#">Report</a> | <a href="#">Save</a></span>
+                        </div>
+                </div>
+            )
+        })
+    )
+}
+const Time = ({time}) => {
     const timeString = moment(time).fromNow();
         return (
             <span className="time">
@@ -87,13 +237,12 @@ const Time = ({ time }) => {
             </span>
         )
 }
-
 class HackerNews extends React.Component {
     render () {
         return (
             <div>
                 <HeadNavBar/>,
-                <Article props={expPost}/>
+                <Article posts={expPost}/>
             </div>
         )
     }
