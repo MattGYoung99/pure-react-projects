22ff97d45397da06b8ab21e2fba482b8b155031e
Matt Young <mattgyoung99@gmail.com>
2018-06-13 06:50:26 -0500

    added items and cartpage

--- shopping-site/src/App.js
+++ shopping-site/src/App.js
@@ -1,26 +1,62 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import Nav from './Nav';
 import ItemPage from './ItemPage';
 import {items} from './static-data';
+import CartPage from './CartPage';
 import './App.css';
 
 class App extends React.Component {
   state = {
-    activeTab: 0
-  };
+    activeTab: 0,
+    cart: []
+  }
 
-  handleTabChange = (index) => {
+  handleAddToCart = (item) => {
     this.setState({
-      activeTab: index
+      cart: [...this.state.cart, item.id]
     });
   }
   renderContent() {
-    switch(this.state.activeTab) {
-      default:
-      case 0: return <ItemPage items={items}/>
-      case 1: return <span>Cart</span>;
+      switch(this.state.activeTab) {
+        default:
+        case 0:
+          return (
+            <ItemPage
+              items={items}
+              onAddToCart={this.handleAddToCart} />
+          );
+        case 1:
+          return this.renderCart();
+      }
     }
-  }
+
+    renderCart() {
+      // Count how many of each item is in the cart
+      let itemCounts = this.state.cart.reduce((itemCounts, itemId) => {
+        itemCounts[itemId] = itemCounts[itemId] || 0;
+        itemCounts[itemId]++;
+        return itemCounts;
+      }, {});
+  
+      // Create an array of items
+      let cartItems = Object.keys(itemCounts).map(itemId => {
+        // Find the item by its id
+        var item = items.find(item =>
+          item.id === parseInt(itemId, 10)
+        );
+  
+        // Create a new "item" that also has a 'count' property
+        return {
+          ...item,
+          count: itemCounts[itemId]
+        }
+      });
+  
+      return (
+        <CartPage items={cartItems} />
+      );
+    }
 
   render() {
     let {activeTab} = this.state;
